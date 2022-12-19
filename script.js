4237ed9783bcba94350b3a40987cf83aa70b5afc
vitor-silva-santos <vithorss2203@gmail.com>
2022-12-19 19:26:54 -0300

    atualizando valores das moedas em tempo real

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const button = document.getElementById("btn-convert")
 const imputReal = document.getElementById("imput-real")
 const select = document.getElementById("currency-select")
-// Currency Value
-const dolar = 5.2
-const euro = 6.2
-const bitcoin = 0.000011
 
-const convertValue = () => {
+const convertValue = async () => {
     const imputReal = document.getElementById("imput-real").value
     const showConvertValue = document.getElementById("convert-value")
+
+    const data = await fetch("http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+   
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high 
+    const bitcoin = data.BTCBRL.high * 1000
 
     if (select.value === "euro") {
         showConvertValue.innerText = new Intl.NumberFormat('de-DE',
@@ -21,7 +23,7 @@ const convertValue = () => {
         ).format(imputReal / dolar)
     }
     if (select.value === "bitcoin") {
-        showConvertValue.innerText = (imputReal * bitcoin).toFixed(6)
+        showConvertValue.innerText = (imputReal / bitcoin).toFixed(6)
     }
 
 
@@ -35,18 +37,15 @@ const changeCurrency = () => {
     if (select.value === "euro") {
         currencyCountry.src = "./assets/country/europe.png"
         currencyName.innerText = "Euro"
-        console.log("Euro: ", euro)
     }
     if (select.value === "dolar-americano") {
         currencyCountry.src = "./assets/country/unitedStates.png"
         currencyName.innerText = "Dólar americano"
-        console.log("Dólar: ", dolar)
     }
 
     if (select.value === "bitcoin") {
         currencyCountry.src = "./assets/country/bitcoin.png"
         currencyName.innerText = "Bitcoin"
-        console.log("Bitcoin: ", bitcoin)
     }
 
     convertValue()
